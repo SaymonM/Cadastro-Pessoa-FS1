6029fa20ce8ce1c8c03d76d58f7c85c1d448f1d4
SaymonM <saymonmeura@hotmail.com>
2022-02-09 09:41:34 -0400

    Encontro Remoto 2

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,16 @@
+using Cadastro_Pessoa_FS1.Interfaces;
+
+namespace Cadastro_Pessoa_FS1.Classes
+{
+    public abstract class Pessoa : IPessoa
+    {
+
+        public string ?nome { get; set;}
+
+        public string ?endereco { get; set;}
+
+        public string ?rendimento { get; set;}
+
+        public abstract float PagarImposto(float rendimento);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using Cadastro_Pessoa_FS1.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.nome = "Teste";
+
+Console.WriteLine(novaPf.nome);
